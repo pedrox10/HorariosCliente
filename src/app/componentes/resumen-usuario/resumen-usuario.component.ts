38288e84e48034645f9f9f392eb273df777f7dff
Pedro Barco Carrasco <barcocarrasco.pedro@gmail.com>
2024-10-21 16:06:43 -0400

    Solucionado error de tipos

--- src/app/componentes/resumen-usuario/resumen-usuario.component.ts
+++ src/app/componentes/resumen-usuario/resumen-usuario.component.ts
@@ -17,39 +17,46 @@ export class ResumenUsuarioComponent {
   public gestion: number = 2024
   public mesActual: number = 7;
   public numDias: number;
-  public dias:string[]= [];
-  private res:string  = "";
+  public dias: string[] = [];
+  private res: string = "";
   private activatedRoute = inject(ActivatedRoute);
   public ip = this.activatedRoute.snapshot.params['ip'];
   public puerto = this.activatedRoute.snapshot.params['puerto'];
   public nombre = this.activatedRoute.snapshot.params['nombre'];
   public meses = env.meses;
-  public feb = [];
+  public feb: string[] = [];
 
   constructor(public terminalService: TerminalService) {
     this.terminalService.getMarcaciones(this.ip, this.puerto).subscribe(
       (data: any) => {
-        console.log(data);
         const aux = data;
-        this.feb = aux.filter(item => /(.*)-02-(.*)/.test(item.timestamp)
+        let cad = this.gestion + "-10-";
+        let re_fecha = new RegExp(cad + '(.*)');
+        let re_ci = new RegExp(this.nombre);
+        aux.forEach((value: any) => {
+          if (re_fecha.test(value.timestamp) && re_ci.test(value.user_id)) {
+            this.feb.push(value);
+          }
+        });
       },
       (error: any) => {
         console.error('An error occurred:', error);
       }
-    );  
+    );
     this.numDias = this.getNumDias(this.mesActual, this.gestion);
     for (var i = 1; i <= this.numDias; i++) {
-      this.dias.push(this.getNombreDia(i).substring(0,3) + " " + i);
+      this.dias.push(this.getNombreDia(i).substring(0, 3) + " " + i);
     }
+    console.log(this.feb);
   }
 
   getNumDias(mes: number, gestion: number) {
     return new Date(gestion, mes, 0).getDate();
   }
 
-  getNombreDia(dia:number) {
-    const birthday = new Date(this.gestion, this.mesActual, dia);
-    const day1 = birthday.getDay();
-    return env.dias[day1];
+  getNombreDia(dia: number) {
+    const fecha = new Date(this.gestion, this.mesActual, dia);
+    const day = fecha.getDay();
+    return env.dias[day];
   }
 }
