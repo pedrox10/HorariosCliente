6f3382fb545c194fc39f34fd23cbf3cd3350f453
pedrox10 <barcocarrasco.pedro@gmail.com>
2024-12-17 01:58:27 -0400

    cambios home 17 Dic

--- src/app/componentes/usuarios/asignar-horarios/asignar-horarios.component.ts
+++ src/app/componentes/usuarios/asignar-horarios/asignar-horarios.component.ts
@@ -1,44 +1,63 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
-import {TerminalService} from "../../../servicios/terminal.service";
-import {env} from "../../../../environments/environments";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, Validators} from "@angular/forms";
 import {ModalService} from "ngx-modal-ease";
 import {Location} from "@angular/common";
 import {Usuario} from "../../../modelos/Usuario";
 import {easepick} from "@easepick/core";
 import {RangePlugin} from "@easepick/range-plugin";
+import {HorarioService} from "../../../servicios/horario.service";
+import {Horario} from "../../../modelos/Horario";
+import {env} from "../../../../environments/environments";
 
 @Component({
   selector: 'app-asignar-horarios',
   standalone: true,
-  imports: [RouterLink, HttpClientModule],
-  providers: [TerminalService],
+  imports: [RouterLink, HttpClientModule, FormsModule],
+  providers: [HorarioService],
   templateUrl: './asignar-horarios.component.html',
   styleUrl: './asignar-horarios.component.css'
 })
-export class AsignarHorariosComponent implements OnInit, AfterViewInit{
+export class AsignarHorariosComponent implements OnInit, AfterViewInit {
   dias = env.dias.map((dia) => dia.toLowerCase());
-  colores = env.colores;
+  horarios: Horario[] = [];
+  jornadaDias: any[] = [];
   formAccion = new FormGroup({});
-  dd_color: any;
   usuarios: Usuario[] = [];
 
-  constructor(private modalService: ModalService) {
-
+  constructor(private modalService: ModalService, public horarioService: HorarioService) {
+    this.horarioService.getHorarios().subscribe(
+      (data: any) => {
+        this.horarios = data;
+        console.log(this.horarios)
+      }, (error: any) => {
+        console.error("Error cargando horarios", error)
+      })
   }
 
   cerrarModal() {
     this.modalService.close();
   }
 
+  seleccionar(ev: any) {
+    let id: number = parseInt(ev.target.value)
+    const index = this.horarios.findIndex(x => x.id === id);
+    let horario = this.horarios[index];
+    document.getElementById("tolerancia")!.innerText = horario.tolerancia + " min.";
+    document.getElementById("color")!.innerText = JSON.parse(horario.color).color;
+    this.jornadaDias = horario.jornadaDias
+    console.log(this.jornadaDias)
+  }
+
   ngOnInit(): void {
-    let data:any = this.modalService.options?.data
-    this.usuarios = data.usuarios;
-    this.usuarios.forEach(usuario => {
-      console.log(usuario.nombre)
-    })
+    let data: any = this.modalService.options?.data
+    if(data){
+      this.usuarios = data.usuarios;
+      this.usuarios.forEach(usuario => {
+        console.log(usuario.nombre)
+      })
+    }
   }
 
   ngAfterViewInit() {
--- src/app/modelos/Horario.ts
+++ src/app/modelos/Horario.ts
@@ -4,13 +4,15 @@ export class Horario {
   tolerancia:number
   color: string
   descripcion: string
+  jornadaDias: any
 
-  constructor(id: number, nombre: string, tolerancia:number, color: string, descripcion: string) {
+  constructor(id: number, nombre: string, tolerancia:number, color: string, descripcion: string, jornadaDias:any) {
     this.id = id;
     this.nombre = nombre;
     this.tolerancia = tolerancia
     this.color = color
     this.descripcion = descripcion
+    this.jornadaDias = jornadaDias
   }
 
 }
